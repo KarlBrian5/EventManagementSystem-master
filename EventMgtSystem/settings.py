243af0ace55cecd40dd90ae6f10f909fe1da84a4
KarlBrian5 <bryanbleinson27@gmail.com>
2022-11-29 16:54:39 +0300

    Email Change

--- EventMgtSystem/settings.py
+++ EventMgtSystem/settings.py
@@ -139,7 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'moeliving96@gmail.com'
+EMAIL_HOST_USER = 'moeliving93@gmail.com'
 EMAIL_HOST_PASSWORD = 'vbuymvxdoulbrpfw'
 
 
